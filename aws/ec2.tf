536ab9bd493d7a16797a89cc28eb8d7a327784fe
Niklas Schmidtmer <niklas@crate.io>
2022-02-23 16:37:50 +0100

    Fix Terraform issue with aws_security_group_rule getting removed on every second run

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -84,6 +84,15 @@ resource "aws_security_group" "cratedb" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = var.ssh_access ? ["0.0.0.0/0"] : []
+    ipv6_cidr_blocks = var.ssh_access ? ["::/0"] : []
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -91,18 +100,6 @@ resource "aws_security_group" "cratedb" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-}
-
-resource "aws_security_group_rule" "ssh" {
-  count = var.ssh_access ? 1 : 0
-
-  security_group_id = aws_security_group.cratedb.id
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
 }
 
 data "aws_ami" "amazon_linux" {
