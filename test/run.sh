38e038658294f0f3a597e231c044a626a53e6fae
Niklas Schmidtmer <hammerhead@users.noreply.github.com>
2024-05-09 22:31:53 +0530

    Update JMX Exporter, add AWS ELB keepalive settings (#118)
    * Update JMX exporter to 1.1.0
    * Align settings with systemd service
    * Add network-related settings for potential AWS ELB connection issues
    * Update dependencies
    * Make test script dynamic

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ "$1" != "aws" ] && [ "$1" != "azure" ]
+then
+  echo "Usage: $0 <aws|azure>"
+  exit
+fi
+
+if [[ "$1" = "aws" ]]; then
+  test_class="TestTerraformAws"
+else
+  test_class="TestTerraformAzure"
+fi
+
 source .env
 
 AWS_TEST_REGION=$AWS_TEST_REGION \
@@ -8,4 +20,4 @@ AWS_TEST_SSH_KEYPAIR=$AWS_TEST_SSH_KEYPAIR \
 AWS_TEST_SUBNET_IDS=$AWS_TEST_SUBNET_IDS \
 AWS_TEST_AVAILABILITY_ZONES=$AWS_TEST_AVAILABILITY_ZONES \
 AZURE_TEST_SUBSCRIPTION_ID=$AZURE_TEST_SUBSCRIPTION_ID \
-go test -timeout 30m -run TestTerraformAzure
+go test -timeout 30m -run "${test_class}"
